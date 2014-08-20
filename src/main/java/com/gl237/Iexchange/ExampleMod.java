dd3d547149ebda6f16aa80879f4c92f8cb0ddd18
Gl237man <gl237man@gmail.com>
2014-08-20 20:57:30 +0400

    Заполнил модинфо

--- src/main/java/com/gl237/Iexchange/ExampleMod.java
+++ src/main/java/com/gl237/Iexchange/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.gl237.Iexchange;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
@@ -8,8 +8,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod1";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "industrialexchange";
+    public static final String VERSION = "0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
