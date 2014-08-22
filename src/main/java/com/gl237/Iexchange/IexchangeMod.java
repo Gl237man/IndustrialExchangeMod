71b9d5b8daa3f77633c54f80693beb7546ee0b69
Vadim <gl237man@gmail.com>
2014-08-22 19:38:09 +0400

    Update IexchangeMod.java

--- src/main/java/com/gl237/Iexchange/IexchangeMod.java
+++ src/main/java/com/gl237/Iexchange/IexchangeMod.java
@@ -21,7 +21,7 @@ public class IexchangeMod
 {
     public static final String MODID = "industrialexchange";
     public static final String VERSION = "0.1";
-    //Ðåãèñòðàöèÿ æèäêîñòåé
+    //Проверка
     public Fluid LEnergyFluid;
     public Fluid LMatterFluid;
     //Ðåãèñòðàöèÿ áëîêîâ æèäêîñòè
