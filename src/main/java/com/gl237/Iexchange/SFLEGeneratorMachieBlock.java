b32885cc0b7a81b827c1b4e5533cf3a7b0beead0
Gl237man <gl237man@gmail.com>
2014-09-19 22:07:42 +0400

    Добавил вываливание предметов при разрушении блока

--- src/main/java/com/gl237/Iexchange/SFLEGeneratorMachieBlock.java
+++ src/main/java/com/gl237/Iexchange/SFLEGeneratorMachieBlock.java
@@ -7,14 +7,18 @@ package com.gl237.Iexchange;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
@@ -54,6 +58,52 @@ public class SFLEGeneratorMachieBlock extends BlockContainer
         return true;
     }
 
+    //Вываливание содержимого блока при разрушении
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int hz)
+    {
+        Random R = new Random();
+        SFLEGeneratorMachieTileEntity tileEntity = (SFLEGeneratorMachieTileEntity)world.getTileEntity(x, y, z);
+
+        if (tileEntity != null)
+        {
+            for (int i1 = 0; i1 < tileEntity.getSizeInventory(); ++i1)
+            {
+                ItemStack itemstack = tileEntity.getStackInSlot(i1);
+
+                if (itemstack != null)
+                {
+                    float f = R.nextFloat() * 0.8F + 0.1F;
+                    float f1 = R.nextFloat() * 0.8F + 0.1F;
+                    EntityItem entityitem;
+
+                    for (float f2 = R.nextFloat() * 0.8F + 0.1F; itemstack.stackSize > 0; world.spawnEntityInWorld(entityitem))
+                    {
+                        int j1 = R.nextInt(21) + 10;
+
+                        if (j1 > itemstack.stackSize)
+                        {
+                            j1 = itemstack.stackSize;
+                        }
+
+                        itemstack.stackSize -= j1;
+                        entityitem = new EntityItem(world, (double)((float)x + f), (double)((float)y + f1), (double)((float)z + f2), new ItemStack(itemstack.getItem(), j1, itemstack.getItemDamage()));
+                        float f3 = 0.05F;
+                        entityitem.motionX = (double)((float)R.nextGaussian() * f3);
+                        entityitem.motionY = (double)((float)R.nextGaussian() * f3 + 0.2F);
+                        entityitem.motionZ = (double)((float)R.nextGaussian() * f3);
+
+                        if (itemstack.hasTagCompound())
+                        {
+                            entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
+                        }
+                    }
+                }
+            }
+        }
+
+        super.breakBlock(world, x, y, z, block, hz);
+    }
 	
 	//Тип предмета при добычи
 	@Override
